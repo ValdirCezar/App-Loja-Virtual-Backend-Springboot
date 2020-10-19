28347c57c82f302d7178f496fe8320c0d43884f0
= <=>
2020-10-19 19:11:04 -0300

    Operação de instanciação automática para Categoria no banco H2

--- src/main/java/com/valdir/mc/McApplication.java
+++ src/main/java/com/valdir/mc/McApplication.java
@@ -1,13 +1,33 @@
 package com.valdir.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.valdir.mc.domain.Categoria;
+import com.valdir.mc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class McApplication {
+public class McApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
