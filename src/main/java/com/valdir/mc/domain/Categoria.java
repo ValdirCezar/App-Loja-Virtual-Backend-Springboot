8fb9ecf04daa0c6332118c547bbbf7212ec4a60b
= <=>
2020-10-20 14:30:34 -0300

    ProdutoRepository e instancias

--- src/main/java/com/valdir/mc/domain/Categoria.java
+++ src/main/java/com/valdir/mc/domain/Categoria.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -19,6 +20,7 @@ public class Categoria implements Serializable {
 	private Integer id;
 	private String nome;
 
+	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 
 	public Categoria() {
--- src/main/java/com/valdir/mc/domain/Produto.java
+++ src/main/java/com/valdir/mc/domain/Produto.java
@@ -8,18 +8,27 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
-public class Produto implements Serializable{
-	
+public class Produto implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private Double preco;
-	
+
+	@ManyToMany
+	@JoinTable(
+		name = "PRODUTO_CATEGORIA", 
+		joinColumns = @JoinColumn(name = "produto_id"), 
+		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+		)
 	private List<Categoria> categorias = new ArrayList<>();
 
 	public Produto() {
