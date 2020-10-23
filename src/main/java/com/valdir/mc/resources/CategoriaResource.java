dfc2f068b0e908ee3b4b5bec0c9808b7fe6bca31
= <=>
2020-10-23 13:57:11 -0300

    Atualizando Categoria com PUT

--- src/main/java/com/valdir/mc/resources/CategoriaResource.java
+++ src/main/java/com/valdir/mc/resources/CategoriaResource.java
@@ -35,5 +35,12 @@ public class CategoriaResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 
 }
