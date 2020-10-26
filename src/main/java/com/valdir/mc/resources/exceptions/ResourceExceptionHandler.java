9486a43bb44298b0745d62bca5b3349e247c83cd
= <=>
2020-10-26 15:07:00 -0300

    Personalizando erro da validação

--- src/main/java/com/valdir/mc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/valdir/mc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,20 +14,34 @@ import com.valdir.mc.services.exceptions.ObjectNotFoubdException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ObjectNotFoubdException.class)
-	public ResponseEntity<StandarError> objectNotFound (ObjectNotFoubdException e, HttpServletRequest request){
-		
+	public ResponseEntity<StandarError> objectNotFound(ObjectNotFoubdException e, HttpServletRequest request) {
+
 		StandarError error = new StandarError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-		
+
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
-	
+
 	@ExceptionHandler(DataIntegrityException.class)
-	public ResponseEntity<StandarError> dataIntegrityException (DataIntegrityException e, HttpServletRequest request){
-		
-		StandarError error = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
-		
+	public ResponseEntity<StandarError> dataIntegrityException(DataIntegrityException e, HttpServletRequest request) {
+
+		StandarError error = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandarError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+		ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação!",
+				System.currentTimeMillis());
+
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			error.addError(x.getField(), x.getDefaultMessage());
+		}
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 
---
+++ src/main/java/com/valdir/mc/resources/exceptions/ValidationError.java
@@ -0,0 +1,30 @@
+package com.valdir.mc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandarError {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		this.errors.add(new FieldMessage(fieldName, message));
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+
+}
