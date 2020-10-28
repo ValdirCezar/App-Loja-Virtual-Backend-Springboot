19bee08a5fb1c81ad823fe653c553eadebb2df7f
= <=>
2020-10-28 16:20:42 -0300

    Validação de e-mail não repetido na atualizacao | Parte 1

---
+++ src/main/java/com/valdir/mc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.valdir.mc.services.validation;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.valdir.mc.domain.Cliente;
+import com.valdir.mc.domain.dto.ClienteDTO;
+import com.valdir.mc.repositories.ClienteRepository;
+import com.valdir.mc.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer urlId = Integer.parseInt(map.get("id"));
+
+		Cliente obj = clienteRepository.findByEmail(objDto.getEmail());
+		if (obj != null) {
+			list.add(new FieldMessage("email", "E-mail já possui cadastro no sistema"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		
+		
+		
+		return list.isEmpty();
+	}
+}
