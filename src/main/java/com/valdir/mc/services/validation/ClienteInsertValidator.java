97b86a54bf62074024132ad1b570547007316be1
= <=>
2020-10-28 16:09:19 -0300

    Validacao customizada | E-mail nao repetido no cadastro de um cliente

--- src/main/java/com/valdir/mc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/valdir/mc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.valdir.mc.domain.Cliente;
 import com.valdir.mc.domain.dto.ClienteNewDTO;
 import com.valdir.mc.domain.enums.TipoCliente;
+import com.valdir.mc.repositories.ClienteRepository;
 import com.valdir.mc.resources.exceptions.FieldMessage;
 import com.valdir.mc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,11 +37,19 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
 		}
 		
+		Cliente obj = clienteRepository.findByEmail(objDto.getEmail());
+		if (obj != null) {
+			list.add(new FieldMessage("email", "E-mail já possui cadastro no sistema"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
 					.addConstraintViolation();
 		}
+		
+		
+		
 		return list.isEmpty();
 	}
 }
