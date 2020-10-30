e617b365e00f59f23abf0cc8cc6266fbcf5ac0a8
= <=>
2020-10-30 07:54:18 -0300

    Classe de configuração para campo adicional no Json de Pedido

--- src/main/java/com/valdir/mc/domain/PagamentoComCartao.java
+++ src/main/java/com/valdir/mc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.valdir.mc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.valdir.mc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 
 	private static final long serialVersionUID = 1L;
