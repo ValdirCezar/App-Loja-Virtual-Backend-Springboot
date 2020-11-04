6331e468350a270f1f6da4bed769fce2a2074739
= <=>
2020-11-04 11:11:41 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/valdir/mc/domain/Cliente.java
+++ src/main/java/com/valdir/mc/domain/Cliente.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.valdir.mc.domain.enums.Perfil;
 import com.valdir.mc.domain.enums.TipoCliente;
 
 @Entity
@@ -36,6 +39,10 @@ public class Cliente implements Serializable {
 	
 	@JsonIgnore
 	private String senha;
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
 
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
@@ -50,6 +57,7 @@ public class Cliente implements Serializable {
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
@@ -60,6 +68,7 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo == null) ? null : tipo.getCodigo();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
@@ -132,6 +141,14 @@ public class Cliente implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCodigo());
 	}
 
 	@Override
--- src/main/java/com/valdir/mc/services/DBService.java
+++ src/main/java/com/valdir/mc/services/DBService.java
@@ -19,6 +19,7 @@ import com.valdir.mc.domain.PagamentoComCartao;
 import com.valdir.mc.domain.Pedido;
 import com.valdir.mc.domain.Produto;
 import com.valdir.mc.domain.enums.EstadoPagamento;
+import com.valdir.mc.domain.enums.Perfil;
 import com.valdir.mc.domain.enums.TipoCliente;
 import com.valdir.mc.repositories.CategoriaRepository;
 import com.valdir.mc.repositories.CidadeRepository;
@@ -120,16 +121,22 @@ public class DBService {
 
 		// ------ Cliente - Endereço -------
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "valdircezar312@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		Cliente cli1 = new Cliente(null, "Maria Silva", "valdircezar312@gmail.com", "51794566066", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("33235656", "25964878"));
+		
+		Cliente cli2 = new Cliente(null, "Enzo Fernandes Goncalves", "fotiwit635@jqjlb.com", "47916024047", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("(21) 7771-8581", "(21) 7771-5291"));
+		cli2.addPerfil(Perfil.ADMIN);
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "387777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Matos", "1022", "Sala 800", "São Gonçalo-RJ", "24473130", cli2, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		// ------ Pedido - Pagamento --------
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
