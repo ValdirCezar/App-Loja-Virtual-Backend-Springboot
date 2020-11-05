eff0e0cc42be8e438ca1d94d9711f47abe0386f0
= <=>
2020-11-05 14:14:27 -0300

    Enviando imagem via endpoint - Parte 1

--- src/main/java/com/valdir/mc/McApplication.java
+++ src/main/java/com/valdir/mc/McApplication.java
@@ -1,26 +1,18 @@
 package com.valdir.mc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.valdir.mc.services.S3Service;
-
 @SpringBootApplication
 public class McApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3service;
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3service.uploadFile("C:\\Users\\valdi\\Documents\\Cursos\\Spring Projects\\pc-gamer.jpg");
 	}
 
 }
