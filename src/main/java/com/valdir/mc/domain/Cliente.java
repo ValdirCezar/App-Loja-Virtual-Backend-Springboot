a8135f954a23241ff396fdcd040dcfe1dded98ad
= <=>
2020-11-05 16:14:38 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/valdir/mc/domain/Cliente.java
+++ src/main/java/com/valdir/mc/domain/Cliente.java
@@ -55,8 +55,6 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 
-	private String imageUrl;
-
 	public Cliente() {
 		super();
 		addPerfil(Perfil.CLIENTE);
@@ -151,14 +149,6 @@ public class Cliente implements Serializable {
 
 	public void addPerfil(Perfil perfil) {
 		perfis.add(perfil.getCodigo());
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
 
 	@Override
--- src/main/java/com/valdir/mc/services/ClienteService.java
+++ src/main/java/com/valdir/mc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.valdir.mc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -7,6 +8,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -39,6 +41,11 @@ public class ClienteService {
 	private BCryptPasswordEncoder pe;
 	@Autowired
 	private S3Service s3service;
+	@Autowired
+	private ImageService imageService;
+
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 
 	public Cliente find(Integer id) {
 
@@ -121,14 +128,11 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 
-		URI uri = s3service.uploadFile(multiPartFile);
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multiPartFile);
+		String fileName = prefix + user.getId() + ".jpg";
 
-		Cliente cli = repo.findByEmail(user.getUsername());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
+		return s3service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 
-		return uri;
 	}
-	
-	
+
 }
