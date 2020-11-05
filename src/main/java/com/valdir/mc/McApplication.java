d39defdec928bc5392f6badfb7e9bc542ea28de8
= <=>
2020-11-05 12:17:43 -0300

    Salvando primeiro arquivo no Amazon S3

--- src/main/java/com/valdir/mc/McApplication.java
+++ src/main/java/com/valdir/mc/McApplication.java
@@ -1,11 +1,17 @@
 package com.valdir.mc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.valdir.mc.services.S3Service;
+
 @SpringBootApplication
 public class McApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3service;
 
 	
 	public static void main(String[] args) {
@@ -14,7 +20,7 @@ public class McApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3service.uploadFile("C:\\Users\\valdi\\Documents\\Cursos\\Spring Projects\\pc-gamer.jpg");
 	}
 
 }
