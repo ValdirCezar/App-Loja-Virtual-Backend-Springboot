b64e31b8a13bab9771d53bcf91002cedaf1ea811
= <=>
2020-11-09 11:33:52 -0300

    Ajuste na busca de cidades

--- src/main/java/com/valdir/mc/services/CidadeService.java
+++ src/main/java/com/valdir/mc/services/CidadeService.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.valdir.mc.domain.Cidade;
+import com.valdir.mc.domain.Estado;
 import com.valdir.mc.repositories.CidadeRepository;
+import com.valdir.mc.services.exceptions.ObjectNotFoubdException;
 
 @Service
 public class CidadeService {
@@ -18,6 +20,9 @@ public class CidadeService {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Cidade> findByEstado(Integer id) {
+		if(repo.findCidades(id).isEmpty()) {
+			throw new ObjectNotFoubdException("Objeto não encontrado! id: " + id + ", Tipo: " + Estado.class.getName());
+		}
 		return repo.findCidades(id);
 	}
 
